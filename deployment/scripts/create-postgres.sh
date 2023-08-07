fb5f1db0724bffdd558fe18650c45ecfdb606463
eya-rabeh <eya.rabeh@esprit.tn>
2023-08-07 16:44:17 +0200

    Create aws rds

--- deployment/scripts/create-postgres.sh
+++ deployment/scripts/create-postgres.sh
@@ -1,11 +1,10 @@
 NAMESPACE=$1
 REPONAME=$2
 
-FILE_PATH=deployment/app-${NAMESPACE}/postgres.yaml
+FILE_PATH=deployment/app-${NAMESPACE}/aws-rds.yaml
 
 
 cp deployment/templates/postgres.yaml $FILE_PATH
-yq --inplace ".metadata.name = \"${REPONAME}-pg\"" $FILE_PATH
-yq --inplace ".spec.id = \"${REPONAME}-pg\"" $FILE_PATH
-yq --inplace ".spec.parameters.namespace = \"${NAMESPACE}\"" $FILE_PATH
-yq --inplace ".spec.writeConnectionSecretToRef.name = \"${REPONAME}-pg\"" $FILE_PATH
+yq --inplace ".metadata.name = \"${REPONAME}-${NAMESPACE}-pg\"" $FILE_PATH
+yq --inplace ".spec.id = \"${REPONAME}-${NAMESPACE}-pg\"" $FILE_PATH
+yq --inplace ".spec.writeConnectionSecretToRef.name = \"${REPONAME}-${NAMESPACE}-pg\"" $FILE_PATH
