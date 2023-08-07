0830e41caa305fc33f030f8c71a13ca3eb5395af
eya-rabeh <eya.rabeh@esprit.tn>
2023-08-07 16:06:18 +0200

    Update create-postgres script and workflow

--- deployment/scripts/create-postgres.sh
+++ deployment/scripts/create-postgres.sh
@@ -1,8 +1,11 @@
 NAMESPACE=$1
-
+REPONAME=$2
 
 FILE_PATH=deployment/app-${NAMESPACE}/postgres.yaml
 
 
 cp deployment/templates/postgres.yaml $FILE_PATH
-yq --inplace ".metadata.namespace = \"${NAMESPACE}\"" $FILE_PATH
+yq --inplace ".metadata.name = \"${REPONAME}-pg\"" $FILE_PATH
+yq --inplace ".spec.id = \"${REPONAME}-pg\"" $FILE_PATH
+yq --inplace ".spec.parameters.namespace = \"${NAMESPACE}\"" $FILE_PATH
+yq --inplace ".spec.writeConnectionSecretToRef.name = \"${REPONAME}-pg\"" $FILE_PATH
