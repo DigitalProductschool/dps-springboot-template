da457a543a5a9be27ee3a48e7168113357ce9991
eya-rabeh <eya.rabeh@esprit.tn>
2023-08-07 17:11:07 +0200

    Update create postgres

---
+++ deployment/scripts/create-postgres.sh
@@ -0,0 +1,16 @@
+ENV=$1
+REPONAME=$2
+PROVIDER=$3
+VERSION=$4
+SIZE=$5
+
+FILE_PATH=deployment/app-${ENV}/${PROVIDER}-pg.yaml
+
+
+cp deployment/templates/postgres.yaml $FILE_PATH
+yq --inplace ".metadata.name = \"${REPONAME}-${ENV}-pg\"" $FILE_PATH
+yq --inplace ".spec.id = \"${REPONAME}-${ENV}-pg\"" $FILE_PATH
+yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH
+yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
+yq --inplace ".spec.parameters.size = \"${SIZE}\"" $FILE_PATH
+yq --inplace ".spec.writeConnectionSecretToRef.name = \"${REPONAME}-${ENV}-pg\"" $FILE_PATH
