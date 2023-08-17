a2eef33b2946487e32b7e154ce7fa6a5ee1973e8
Eya Rabeh <48928985+eya-rabeh@users.noreply.github.com>
2023-08-17 15:04:37 +0200

    Update create-postgres.sh

--- deployment/scripts/create-postgres.sh
+++ deployment/scripts/create-postgres.sh
@@ -9,6 +9,7 @@ FILE_PATH=deployment/app-${ENV}/${PROVIDER}-pg.yaml
 
 cp deployment/templates/postgres.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
+yq --inplace ".metadata.namespace = \"${ENV}\"" $FILE_PATH
 yq --inplace ".spec.id = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
 yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH
 yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
