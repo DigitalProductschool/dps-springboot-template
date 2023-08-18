f9f36ab95168ba600e1002aacaa85be2f3ecdcc2
Eya Rabeh <48928985+eya-rabeh@users.noreply.github.com>
2023-08-18 11:11:38 +0200

    Update create-postgres.sh

--- deployment/scripts/create-postgres.sh
+++ deployment/scripts/create-postgres.sh
@@ -12,4 +12,3 @@ yq --inplace ".spec.id = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
 yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH
 yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
 yq --inplace ".spec.parameters.size = \"${SIZE}\"" $FILE_PATH
-yq --inplace ".spec.writeConnectionSecretToRef.name = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
