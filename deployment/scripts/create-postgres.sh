c343f3dc8a3a60659ab34a87b48cc8789440adc8
eya-rabeh <eya.rabeh@esprit.tn>
2023-09-29 15:03:40 +0200

    Fix typo

--- deployment/scripts/create-postgres.sh
+++ deployment/scripts/create-postgres.sh
@@ -5,14 +5,13 @@ VERSION=$4
 SIZE=$5
 ACCOUNT=$6
 
-FILE_PATH=deployment/app-${ENV}/${PROVIDER}-pg.yaml
+FILE_PATH=deployment/app-${ENV}/${ACCOUNT}-${PROVIDER}-pg.yaml
 
 cp deployment/templates/postgres.yaml $FILE_PATH
-yq --inplace ".spec..target.name = \"${REPONAME}-pg-${ENV}-password\"" $FILE_PATH
+yq --inplace ".spec.target.name = \"${REPONAME}-pg-${ENV}-password\"" $FILE_PATH
 yq --inplace ".metadata.name = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
 yq --inplace ".spec.id = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
 yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH
 yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
 yq --inplace ".spec.parameters.size = \"${SIZE}\"" $FILE_PATH
 yq --inplace ".spec.parameters.providerName = \"${ACCOUNT}\"" $FILE_PATH
-
