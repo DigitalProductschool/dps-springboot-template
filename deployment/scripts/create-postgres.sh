442bb8292410cd25372e3a6a11ee427584b80432
eya-rabeh <eya.rabeh@esprit.tn>
2023-09-29 15:29:14 +0200

    Seperate secret and claim

--- deployment/scripts/create-postgres.sh
+++ deployment/scripts/create-postgres.sh
@@ -5,13 +5,17 @@ VERSION=$4
 SIZE=$5
 ACCOUNT=$6
 
-FILE_PATH=deployment/app-${ENV}/${ACCOUNT}-${PROVIDER}-pg.yaml
+FILE_PATH=deployment/app-${ENV}/${ACCOUNT}-${PROVIDER}-pg-password.yaml
+FILE_PATH2=deployment/app-${ENV}/${ACCOUNT}-${PROVIDER}-pg.yaml
 
-cp deployment/templates/postgres.yaml $FILE_PATH
+cp deployment/templates/secret.yaml $FILE_PATH
 yq --inplace ".spec.target.name = \"${REPONAME}-pg-${ENV}-password\"" $FILE_PATH
 yq --inplace ".metadata.name = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
-yq --inplace ".spec.id = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH
-yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH
-yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH
-yq --inplace ".spec.parameters.size = \"${SIZE}\"" $FILE_PATH
-yq --inplace ".spec.parameters.providerName = \"${ACCOUNT}\"" $FILE_PATH
+
+cp deployment/templates/postgres.yaml $FILE_PATH2
+yq --inplace ".metadata.name = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH2
+yq --inplace ".spec.id = \"${REPONAME}-pg-${ENV}\"" $FILE_PATH2
+yq --inplace ".spec.compositionSelector.matchLabels.provider = \"${PROVIDER}\"" $FILE_PATH2
+yq --inplace ".spec.parameters.version = \"${VERSION}\"" $FILE_PATH2
+yq --inplace ".spec.parameters.size = \"${SIZE}\"" $FILE_PATH2
+yq --inplace ".spec.parameters.providerName = \"${ACCOUNT}\"" $FILE_PATH2
