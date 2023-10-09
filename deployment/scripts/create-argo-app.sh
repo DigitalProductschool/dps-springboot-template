a49d593fc7334127ff25671f7aa0bb08ccb7e315
eya-rabeh <eya.rabeh@esprit.tn>
2023-10-09 17:59:22 +0200

    Add missing RepoURL 🫣

--- deployment/scripts/create-argo-app.sh
+++ deployment/scripts/create-argo-app.sh
@@ -1,6 +1,7 @@
 # Assign arguments to variables
 ENV=$1
 CONTEXT=$2
+REPO=$3
 
 FILE_PATH=deployment/app-${ENV}/app.yaml
 
@@ -16,5 +17,6 @@ fi
 
 yq --inplace ".metadata.name = \"${CONTEXT}-${ENV}\"" $FILE_PATH
 yq --inplace ".spec.project = \"${ENVIRONMENT}\"" $FILE_PATH
+yq --inplace ".spec.source.repoURL = \"https://github.com/DigitalProductschool/${REPO}.git\"" $FILE_PATH
 yq --inplace ".spec.source.path = \"${DIRECTORY}/${CONTEXT}\"" $FILE_PATH
 yq --inplace ".spec.destination.namespace = \"${ENV}\"" $FILE_PATH
