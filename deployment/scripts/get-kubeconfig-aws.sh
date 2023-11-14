933918e8a742cb5d587e47d3215b679c85162e4b
eya-rabeh <eya.rabeh@esprit.tn>
2023-11-14 16:03:26 +0100

    Remove get kubeconfig script 🗑️

--- deployment/scripts/get-kubeconfig-aws.sh
+++
@@ -1,6 +0,0 @@
-CLUSTER_NAME=$1
-
-aws eks update-kubeconfig --region eu-central-1 \
-    --name $CLUSTER_NAME --kubeconfig $PWD/kubeconfig-$CLUSTER_NAME.yaml
-
-chmod 600 kubeconfig-$CLUSTER_NAME.yaml
